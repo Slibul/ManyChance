e7d2ac45b61b8fc18b026b938bd59c5c89539f84
Slibul <wlalsaka000@gmail.com>
2024-05-06 15:42:00 +0900

    a

--- Plugin.cs
+++ Plugin.cs
@@ -6,20 +6,21 @@ using MachineChance;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
-    public static ConfigEntry<Rarity> Rare;
 
     public enum Rarity
     {
-        commond = 1, 
-        rare = 2, 
+        commond = 1,
+        rare = 2,
         uniqe = 3,
         Legend = 4,
         ALL = 999
     }
 
+    public static ConfigEntry<Rarity> Rare;
+
     private void Awake()
     {
-        Rare = Config.Bind<Rarity>("General", "나올 레어도", Rarity.commond, "원하시는걸 선택해주세요.");
+        Rare = Config.Bind<Rarity>("General", "나올 레어도", Rarity.ALL, "원하시는걸 선택해주세요.");
         Harmony.CreateAndPatchAll(typeof(MachineChanced));
         Logger.LogInfo($"Mod {MyPluginInfo.PLUGIN_GUID} is loaded!");
     }
